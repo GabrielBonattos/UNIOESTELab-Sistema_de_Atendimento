60df8e28562117b827397a8f975d319e93a7768d
Gabriel Bonatto <gabrielbonattos101@gmail.com>
2024-08-01 21:25:29 -0300

    wip

--- main.cpp
+++ main.cpp
@@ -7,17 +7,25 @@ public:
     MyFrame(const wxString& title);
 
 private:
-    void OnGenerateSenha(wxCommandEvent& event);
+    void OnGerarSenha(wxCommandEvent& event);
+    void OnChamadoSenha(wxCommandEvent& event);
+    void OnChamadoSenhaPreferencial(wxCommandEvent& event);
+    void OnEncerrarAtendimento(wxCommandEvent& event);
 
-    wxStaticText* m_lastCalled;
-    wxButton* m_button;
+    //declarados global
     wxListBox* m_senhaList;
-
+    wxListBox* senhaPreferencialList;
+    wxStaticText* senha;
+    wxArrayString* senhasPreferenciais;
+    wxList* senhaPreferencialList2;
     wxDECLARE_EVENT_TABLE();
 };
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_BUTTON(10001, MyFrame::OnGenerateSenha)
+    EVT_BUTTON(10001, MyFrame::OnGerarSenha)
+    EVT_BUTTON(2, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(3, MyFrame::OnEncerrarAtendimento)
+    EVT_BUTTON(4, MyFrame::OnChamadoSenhaPreferencial)
 wxEND_EVENT_TABLE()
 
 class MyApp : public wxApp {
@@ -28,7 +36,7 @@ public:
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-    MyFrame* frame = new MyFrame("Gerador de Senhas");
+    MyFrame* frame = new MyFrame("UNIOESTELab - Sistema de atendimento em laboratorio");
     frame->Show(true);
     return true;
 }
@@ -37,25 +45,61 @@ bool MyApp::OnInit() {
 
 MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600)) {
     wxPanel* panel = new wxPanel(this, wxID_ANY);
-    m_lastCalled = new wxStaticText(panel, wxID_ANY, wxT("Últimas Chamadas:"), wxPoint(0, 20),wxSize(400, -1), wxALIGN_CENTRE_HORIZONTAL);
+    //wxStaticText* ultimoschamados = new wxStaticText(panel, wxID_ANY, wxT("Últimas Chamadas: "), wxPoint(0, 20),wxSize(400, -1), wxALIGN_CENTRE_HORIZONTAL);
+    senha = new wxStaticText(panel, wxID_ANY, "Ultimos chamados: ", wxPoint(0, 20), wxSize(310, -1), wxALIGN_RIGHT);
     m_senhaList = new wxListBox(panel, wxID_ANY, wxPoint(20, 50), wxSize(350, 150));
-    m_button = new wxButton(panel, 10001, wxT("Gerar Senha"), wxPoint(20, 220));
-    wxButton* criarGuiche = new wxButton(panel, wxID_ANY, wxT("Gerar Guiche"), wxPoint(250, 220));
+    senhaPreferencialList = new wxListBox(panel, wxID_ANY, wxPoint(-9999, 9999));
+    wxButton* gerarSenha = new wxButton(panel, 10001, wxT("Gerar Senha"), wxPoint(20, 220));
+    wxButton* chamarSenha = new wxButton(panel, 2, wxT("Chamar Senha"), wxPoint(120, 220));
+    wxButton* encerrarAtendimento = new wxButton(panel, 3, wxT("Encerrar Atendimento"), wxPoint(230, 220));
+    wxButton* ChamarSenhaPreferencial = new wxButton(panel, 4, wxT("Chamar senha preferencial"), wxPoint(390,220));
 
+    CreateStatusBar();
     srand((unsigned) time(0));  // Inicializa a semente do gerador de números aleatórios
 }
 
-void MyFrame::OnGenerateSenha(wxCommandEvent& event) {
-    int tipo = rand() % 2;  // 0 para não-preferencial, 1 para preferencial
-    static int contadorNaoPref = 100;
-    static int contadorPref = 100;
+void MyFrame::OnChamadoSenha(wxCommandEvent &event) {
+    int pegarUltimo = m_senhaList->GetCount();
+    // wxLogStatus(m_senhaList->GetString(pegarUltimo - 1));
 
-    wxString senha;
-    if (tipo == 0) {
-        senha = wxString::Format("N%d", ++contadorNaoPref);
-    } else {
-        senha = wxString::Format("P%d", ++contadorPref);
+    if(pegarUltimo > 0) {
+        senha->SetLabel("Ultimos chamados: " + m_senhaList->GetString(pegarUltimo - 1));
+    }
+}
+
+void MyFrame::OnChamadoSenhaPreferencial(wxCommandEvent &event) {
+    int pegarUltimo = senhaPreferencialList->GetCount();
+    if(pegarUltimo > 0) {
+        senha->SetLabel("Ultimos chamados: " + senhaPreferencialList->GetString(pegarUltimo - 1));
+    }
+}
+
+
+void MyFrame::OnEncerrarAtendimento(wxCommandEvent &event) {
+    int pegarUltimo = m_senhaList->GetCount();
+    wxLogStatus("clickado");
+
+
+    if(pegarUltimo > 0) {
+        m_senhaList->Delete(pegarUltimo - 1);
     }
 
+}
+
+
+
+void MyFrame::OnGerarSenha(wxCommandEvent& event) {
+    int tipo = rand() % 5;
+    static int contadorNaoPref = 0;
+    static int contadorPref = 0;
+
+    wxString senha;
+    if (tipo == 0 || tipo == 1) {
+        senha = wxString::Format("NOP%03d", ++contadorNaoPref);
+    } else {
+        senha = wxString::Format("PRI%03d", ++contadorPref);
+        senhaPreferencialList->InsertItems(1, &senha, 0);
+        senhasPreferenciais->Add(senha);
+    }
     m_senhaList->InsertItems(1, &senha, 0);
 }
