68f13097eb147d62514ec790f31f4b1d7156ba6d
Gabriel Bonatto <gabrielbonattos101@gmail.com>
2024-08-04 19:29:20 -0300

    Criação de guichês e modo tela cheia

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ public:
 
 private:
     void OnGerarSenha(wxCommandEvent& event);
+    void OnGerarSenhaPreferencial(wxCommandEvent& event);
     void OnChamadoSenha(wxCommandEvent& event);
     void OnChamadoSenhaPreferencial(wxCommandEvent& event);
     void OnEncerrarAtendimento(wxCommandEvent& event);
@@ -16,14 +17,31 @@ private:
     wxListBox* senhaList;
     wxListBox* senhaPreferencialList;
     wxStaticText* senha;
+    wxStaticText* guiche[4];
     wxDECLARE_EVENT_TABLE();
+
+    int senhaAtual[4];
 };
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_BUTTON(10001, MyFrame::OnGerarSenha)
-    EVT_BUTTON(2, MyFrame::OnChamadoSenha)
-    EVT_BUTTON(3, MyFrame::OnEncerrarAtendimento)
-    EVT_BUTTON(4, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(2, MyFrame::OnGerarSenha)
+    EVT_BUTTON(3, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(4, MyFrame::OnEncerrarAtendimento)
+    EVT_BUTTON(5, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(6, MyFrame::OnGerarSenhaPreferencial)
+    EVT_BUTTON(10, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(11, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(12, MyFrame::OnEncerrarAtendimento)
+    EVT_BUTTON(20, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(21, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(22, MyFrame::OnEncerrarAtendimento)
+    EVT_BUTTON(30, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(31, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(32, MyFrame::OnEncerrarAtendimento)
+    EVT_BUTTON(40, MyFrame::OnChamadoSenha)
+    EVT_BUTTON(41, MyFrame::OnChamadoSenhaPreferencial)
+    EVT_BUTTON(42, MyFrame::OnEncerrarAtendimento)
+
 wxEND_EVENT_TABLE()
 
 class MyApp : public wxApp {
@@ -44,14 +62,28 @@ bool MyApp::OnInit() {
 MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600)) {
     wxPanel* panel = new wxPanel(this, wxID_ANY);
     //wxStaticText* ultimoschamados = new wxStaticText(panel, wxID_ANY, wxT("Últimas Chamadas: "), wxPoint(0, 20),wxSize(400, -1), wxALIGN_CENTRE_HORIZONTAL);
-    senha = new wxStaticText(panel, wxID_ANY, "Ultimos chamados: ", wxPoint(0, 20), wxSize(310, -1), wxALIGN_RIGHT);
-    senhaList = new wxListBox(panel, wxID_ANY, wxPoint(20, 50), wxSize(350, 150));
+    senha = new wxStaticText(panel, wxID_ANY, "Ultimo chamado: ", wxPoint(600, 20), wxSize(460, 200), wxALIGN_RIGHT);
+    senha->SetFont(wxFont(20, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+    senhaList = new wxListBox(panel, wxID_ANY, wxPoint(650, 100), wxSize(600, 500));
     senhaPreferencialList = new wxListBox(panel, wxID_ANY, wxPoint(-9999, 9999));
-    wxButton* gerarSenha = new wxButton(panel, 10001, wxT("Gerar Senha"), wxPoint(20, 220));
-    wxButton* chamarSenha = new wxButton(panel, 2, wxT("Chamar Senha"), wxPoint(120, 220));
-    wxButton* encerrarAtendimento = new wxButton(panel, 3, wxT("Encerrar Atendimento"), wxPoint(230, 220));
-    wxButton* ChamarSenhaPreferencial = new wxButton(panel, 4, wxT("Chamar senha preferencial"), wxPoint(390,220));
+    wxButton* gerarSenha = new wxButton(panel, 2, wxT("Gerar Senha"), wxPoint(20, 220));
+    wxButton* chamarSenha = new wxButton(panel, 3, wxT("Chamar Senha"), wxPoint(120, 220));
+    wxButton* encerrarAtendimento = new wxButton(panel, 4, wxT("Encerrar Atendimento"), wxPoint(230, 220));
+    wxButton* ChamarSenhaPreferencial = new wxButton(panel, 5, wxT("Chamar Senha Preferencial"), wxPoint(390,220));
+    wxButton* gerarSenhaPreferencial = new wxButton(panel, 6, wxT("Gerar Senha Preferencial"), wxPoint(20,270));
 
+    //guiches
+    for (int i = 0; i < 4; ++i) {
+        guiche[i] = new wxStaticText(panel, wxID_ANY, wxString::Format("Guiche %d", i+1), wxPoint(1400, 50 + i * 150), wxSize(310, 60));
+        guiche[i]->SetFont(wxFont(15, wxFONTFAMILY_MODERN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+
+        new wxButton(panel, 10 + i * 10, wxT("Chamar Senha"), wxPoint(1400, 100 + i * 150));
+        new wxButton(panel, 11 + i * 10, wxT("Chamar Senha Preferencial"), wxPoint(1510, 100 + i * 150));
+        new wxButton(panel, 12 + i * 10, wxT("Encerrar Atendimento"), wxPoint(1700, 100 + i * 150));
+    }
+
+    this->ShowFullScreen(true);
+    //this->ShowFullScreen(true, wxFULLSCREEN_NOSTATUSBAR || wxFULLSCREEN_NOBORDER || wxFULLSCREEN_NOCAPTION);
     CreateStatusBar();
     srand((unsigned) time(0));  // Inicializa a semente do gerador de números aleatórios
 }
@@ -61,14 +93,14 @@ void MyFrame::OnChamadoSenha(wxCommandEvent &event) {
     // wxLogStatus(m_senhaList->GetString(pegarUltimo - 1));
 
     if(pegarUltimo > 0) {
-        senha->SetLabel("Ultimos chamados: " + senhaList->GetString(pegarUltimo - 1));
+        //guiche->SetLabel("Guiche 01: " + senhaList->GetString(pegarUltimo - 1));
     }
 }
 
 void MyFrame::OnChamadoSenhaPreferencial(wxCommandEvent &event) {
     int pegarUltimo = senhaPreferencialList->GetCount();
     if(pegarUltimo > 0) {
-        senha->SetLabel("Ultimos chamados: " + senhaPreferencialList->GetString(pegarUltimo - 1));
+        senha->SetLabel("Ultimo chamado: " + senhaPreferencialList->GetString(pegarUltimo - 1));
     }
 }
 
@@ -84,19 +116,17 @@ void MyFrame::OnEncerrarAtendimento(wxCommandEvent &event) {
 
 }
 
-
-
 void MyFrame::OnGerarSenha(wxCommandEvent& event) {
-    int tipo = rand() % 5;
     static int contadorNaoPref = 0;
-    static int contadorPref = 0;
-
     wxString senha;
-    if (tipo == 0 || tipo == 1) {
-        senha = wxString::Format("NOP%03d", ++contadorNaoPref);
-    } else {
-        senha = wxString::Format("PRI%03d", ++contadorPref);
-        senhaPreferencialList->InsertItems(1, &senha, 0);
-    }
+    senha = wxString::Format("NOP%03d", ++contadorNaoPref);
+    senhaList->InsertItems(1, &senha, 0);
+}
+
+void MyFrame::OnGerarSenhaPreferencial(wxCommandEvent& event) {
+    static int contadorPref = 0;
+    wxString senha;
+    senha = wxString::Format("PRI%03d", ++contadorPref);
+    senhaPreferencialList->InsertItems(1, &senha, 0);
     senhaList->InsertItems(1, &senha, 0);
 }
